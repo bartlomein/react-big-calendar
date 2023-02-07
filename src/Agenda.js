027bd9f49c2c2e0b73403709f8ecddffbc659e90
Bart Tyrpien <bartlomein123@gmail.com>
2023-02-07 07:03:33 -0500

    test

--- src/Agenda.js
+++ src/Agenda.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 import React, { useRef, useEffect } from 'react'
 import addClass from 'dom-helpers/addClass'
 import removeClass from 'dom-helpers/removeClass'
-import getWidth from 'dom-helpers/width'
+
 import scrollbarSize from 'dom-helpers/scrollbarSize'
 
 import { navigate } from './utils/constants'
@@ -26,6 +26,12 @@ function Agenda({
   const timeColRef = useRef(null)
   const contentRef = useRef(null)
   const tbodyRef = useRef(null)
+
+  const eventRef = useRef(null)
+  const actualRef = useRef(null)
+  const estimateRef = useRef(null)
+  const changeRef = useRef(null)
+  const previousRef = useRef(null)
 
   useEffect(() => {
     _adjustHeader()
@@ -71,14 +77,18 @@ function Agenda({
         )
 
       return (
-        <tr
+        <div
           key={dayKey + '_' + idx}
-          className={userProps.className}
+          className={`agenda-grid-top-header ${first && `agenda-topLine`}`}
           style={userProps.style}
         >
-          {first}
-          <td className="rbc-agenda-time-cell">{timeRangeLabel(day, event)}</td>
-          <td
+          <div className="agenda-day-container">
+            {first && <span className="agenda-day">{first}</span>}
+          </div>
+          <div className="rbc-agenda-time-cell">
+            {timeRangeLabel(day, event)}
+          </div>
+          <div
             className="rbc-agenda-event-cell"
             onClick={(e) => onSelectEvent && onSelectEvent(event, e)}
             onDoubleClick={(e) =>
@@ -86,8 +96,29 @@ function Agenda({
             }
           >
             {Event ? <Event event={event} title={title} /> : title}
-          </td>
-        </tr>
+          </div>
+
+          <div
+            className={`${event?.actual ? `agenda-actual` : `actual-empty`}`}
+          >
+            {event?.actual ? event?.actual?.toFixed(2) : '-'}
+          </div>
+          <div
+            className={`${
+              event?.estimate ? `agenda-estimate` : `estimate-empty`
+            }`}
+          >
+            {event?.estimate ? event?.estimate?.toFixed(2) : '-'}
+          </div>
+          <div
+            className={`${event?.change ? `agenda-change` : `change-empty`}`}
+          >
+            {event?.change ? event?.change?.toFixed(2) : '-'}
+          </div>
+          <div className={`${event.previous ? `agenda-prev` : `prev-empty`}`}>
+            {event?.previous ? event?.previous?.toFixed(2) : '-'}
+          </div>
+        </div>
       )
     }, [])
   }
@@ -103,12 +134,12 @@ function Agenda({
     if (!accessors.allDay(event)) {
       if (localizer.eq(start, end)) {
         label = localizer.format(start, 'agendaTimeFormat')
-      } else if (localizer.isSameDate(start, end)) {
-        label = localizer.format({ start, end }, 'agendaTimeRangeFormat')
+      } else if (localizer.isSameDate(start)) {
+        label = localizer.format({ start }, 'agendaTimeRangeFormat')
       } else if (localizer.isSameDate(day, start)) {
         label = localizer.format(start, 'agendaTimeFormat')
-      } else if (localizer.isSameDate(day, end)) {
-        label = localizer.format(end, 'agendaTimeFormat')
+      } else if (localizer.isSameDate(day)) {
+        // label = localizer.format(end, 'agendaTimeFormat')
       }
     }
 
@@ -137,15 +168,13 @@ function Agenda({
     let isOverflowing =
       contentRef.current.scrollHeight > contentRef.current.clientHeight
 
-    let _widths = []
-    let widths = _widths
-
-    _widths = [getWidth(firstRow.children[0]), getWidth(firstRow.children[1])]
-
-    if (widths[0] !== _widths[0] || widths[1] !== _widths[1]) {
-      dateColRef.current.style.width = _widths[0] + 'px'
-      timeColRef.current.style.width = _widths[1] + 'px'
-    }
+    // dateColRef.current.style.width = _widths[0] + 'px'
+    // timeColRef.current.style.width = _widths[1] + 'px'
+    // eventRef.current.style.width = _widths[2] + 'px'
+    // actualRef.current.style.width = _widths[3] + 'px'
+    // estimateRef.current.style.width = _widths[4] + 'px'
+    // changeRef.current.style.width = _widths[5] + 'px'
+    // previousRef.current.style.width = _widths[6] + 'px'
 
     if (isOverflowing) {
       addClass(header, 'rbc-header-overflowing')
@@ -176,25 +205,40 @@ function Agenda({
     <div className="rbc-agenda-view">
       {events.length !== 0 ? (
         <React.Fragment>
-          <table ref={headerRef} className="rbc-agenda-table">
-            <thead>
-              <tr>
-                <th className="rbc-header" ref={dateColRef}>
+          <div ref={headerRef} className="rbc-agenda-table ">
+            <div>
+              <div className="agenda-grid-top-header">
+                <div className="rbc-header" ref={dateColRef}>
                   {messages.date}
-                </th>
-                <th className="rbc-header" ref={timeColRef}>
+                </div>
+                <div className="rbc-header" ref={timeColRef}>
                   {messages.time}
-                </th>
-                <th className="rbc-header">{messages.event}</th>
-              </tr>
-            </thead>
-          </table>
+                </div>
+                <div className="rbc-header" ref={eventRef}>
+                  {messages.event}
+                </div>
+
+                <div className="rbc-header agenda" ref={actualRef}>
+                  Actual
+                </div>
+                <div className="rbc-header agenda" ref={estimateRef}>
+                  Est
+                </div>
+                <div className="rbc-header agenda" ref={changeRef}>
+                  Change
+                </div>
+                <div className="rbc-header agenda" ref={previousRef}>
+                  Prev
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="rbc-agenda-content" ref={contentRef}>
-            <table className="rbc-agenda-table">
-              <tbody ref={tbodyRef}>
+            <div className="rbc-agenda-table">
+              <div ref={tbodyRef}>
                 {range.map((day, idx) => renderDay(day, events, idx))}
-              </tbody>
-            </table>
+              </div>
+            </div>
           </div>
         </React.Fragment>
       ) : (
